0f9f7bacffcdd30b39def3ffae30b6e474d5a24e
Rennan Nascimento <rnnnsanttos@gmail.com>
2025-09-19 17:41:49 -0400

    Ultimas Funcionalidades

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
-let amigos = [];
+console.log('Arquivo app.js carregado com sucesso!');
 
+let amigos = [];
 function adicionarAmigo() {
     const inputAmigo =  document.getElementById('amigo');
     const nomeAmigo = inputAmigo.value.trim();
-} 
-
-if (nomeAmigo === '') {
-    alert("Por favor, insira um nome.");
-    return;
-} 
-
+ 
+// Validação básica
+    if (nomeAmigo === '') {
+        alert('Por favor, digite um nome.');
+        return;
+}  
+    if (amigos.includes(nomeAmigo)) {
+        alert('Este nome já foi adicionado!');
+        return;
+ }
+ // Adicionar ao array
     amigos.push(nomeAmigo);
-    inputAmigo.value = '';
+    
+// Atualizar a lista visual
     atualizarListaAmigos();
-
+    
+// Limpar o input
+    inputAmigo.value = '';
+    inputAmigo.focus();
+}
+// Função para atualizar a lista visual
 function atualizarListaAmigos() {
     const listaAmigos = document.getElementById('listaAmigos');
-       listaAmigos.innerHTML = ""; 
+       listaAmigos.innerHTML = ''; 
 
-    amigos.forEach(amigo=> {
+amigos.forEach(amigo=> {
        listaAmigos.innerHTML += `<li>${amigo}</>;</li>`;
     });
 }
 
+function sortearAmigo() {
+// Verifica se há amigos dispoiníveis
+    if (amigos.length === 0) {
+        alert("Não há amigos para sortear!Adicione alguns nomes primeiro.");
+    return;
+    }
 
+// Gerar um índice aleatório
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
 
+// Obter o nome sorteado
+    const amigoSorteado = amigos[indiceAleatorio];
 
-
-
+ // Mostrar o resultado
+    const resultadoElemento = document.getElementById('resultado');
+    resultadoElemento.innerHTML = `<li>Amigo sorteado: <strong>${amigoSorteado}</strong></li>`;
+}
 
 
 
