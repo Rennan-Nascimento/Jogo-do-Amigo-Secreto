6fbff8dab4c20eeb0390f8cf7d36dd7c6569866a
Rennan Nascimento <rnnnsanttos@gmail.com>
2025-09-19 10:20:17 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    const inputAmigo =  document.getElementById('amigo');
+    const nomeAmigo = inputAmigo.value.trim();
+} 
+
+if (nomeAmigo === '') {
+    alert("Por favor, insira um nome.");
+    return;
+} 
+
+    amigos.push(nomeAmigo);
+    inputAmigo.value = '';
+    atualizarListaAmigos();
+
+function atualizarListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+       listaAmigos.innerHTML = ""; 
+
+    amigos.forEach(amigo=> {
+       listaAmigos.innerHTML += `<li>${amigo}</>;</li>`;
+    });
+}
+
+
+
+
+
+
+
+
+
+
